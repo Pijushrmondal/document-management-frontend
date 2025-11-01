c63ca2960caa1eb0fd72bd393ff611d06385efb0
Pijush Ray Mondal <pijush@antino.com>
2025-11-01 22:37:03 +0530

    React ap setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-red-400">home page</p>
+    </>
+  );
+}
+
+export default App;
